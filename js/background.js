0f15a257538f8e5a90219d6ca36fc1b02cc2772e
munyeol-Yoon <gmakin36@gmail.com>
2022-11-06 18:54:16 +0900

    t

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ const bgImage = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 const paintImage = () => {
   const random = handleRandom();
-  $body.style.backgroundImage = `url(js-todo/images/Cwhale${random}.jpg)`;
+  $body.style.backgroundImage = `url(./images/Cwhale${random}.jpg)`;
 };
 //`url(../images/Cwhale${random}.jpg)`
 const handleRandom = () => {
